a03a864a82b94796e36f6e15b251dce3b51ea87b
Soichi Hayashi <hayashis@iu.edu>
2016-03-30 15:44:01 +0000

    added basic .nii extension check

--- import.py
+++ import.py
@@ -2,6 +2,7 @@
 import os
 import json
 import sys
+import requests
 
 #dump env .. for debuggin
 for e in os.environ:
@@ -14,9 +15,14 @@ config=json.loads(config_json)
 products = []
 
 for file in config["files"]:
-    print file["filename"]
-    print file["type"]
-    #TODO..
+    #print file["filename"]
+    #print file["type"]
+    status = "Checking "+file["filename"]
+    requests.post(os.environ["SCA_PROGRESS_URL"], data='{"status":"'+status+'"}')
+
+    #TODO - for now, I am just going to check for file extension.. In the future, I will implement something a bit more capable
+    if file["filename"].endswith(".nii"):
+        products[] = file
 
 #output products.json
 with open('products.json', 'w') as out:
