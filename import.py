d0c2104a3a8ea36b3f8314dd252ca9ea9bb509e9
Soichi Hayashi <hayashis@iu.edu>
2016-03-30 15:35:27 +0000

    added a bit more meats to import.sh

--- import.py
+++ import.py
@@ -1,3 +1,25 @@
 #!/bin/env python
 import os
-print os.environ
+import json
+import sys
+
+#dump env .. for debuggin
+for e in os.environ:
+    print e, os.environ[e]
+
+#load config.json
+config_json=open("config.json").read()
+config=json.loads(config_json)
+
+products = []
+
+for file in config["files"]:
+    print file["filename"]
+    print file["type"]
+    #TODO..
+
+#output products.json
+with open('products.json', 'w') as out:
+    json.dump(products, out)
+
+
